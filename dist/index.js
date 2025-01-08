63eba4b919b420c884f93abd8ed6c190273fc19e
sohag47 <minhazul.islamcse@gmail.com>
2025-01-08 13:38:27 +0600

    initialize TypeScript project with basic age calculation and launch configuration

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let age = 20;
+if (age) {
+    age += 10;
+}
+console.log("age :>> ", age);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/** @format */
+
+let age: number = 20;
+
+if (age) {
+  age += 10;
+}
+console.log("age :>> ", age);
