aeefa81c9c27cf5a0938a3d05f560d8ed689cd1d
sohag47 <minhazul.islamcse@gmail.com>
2025-09-24 16:35:26 +0600

    add enums

--- src/basic-types.ts
+++ src/basic-types.ts
@@ -1,13 +1,13 @@
 /* eslint-disable no-console */
 // Learn basic types: string, number, boolean, array, tuple, enum
 export default function BasicTypes(): void {
-  //? Primitive Types : string, number, boolean
+  //! Primitive Types : string, number, boolean
   const name: string = 'Minhazul Islam Sohag';
   const age: number = 20;
   const isStudent: boolean = true;
   console.log('Primitive Types', { name, age, isStudent });
 
-  //? number
+  //! number
   const pi: number = 3.1416;
   const hex: number = 0xff; // Hexadecimal
   const binary: number = 0b1010; // Binary
@@ -15,17 +15,16 @@ export default function BasicTypes(): void {
 
   console.log('Numbers', { age, pi, hex, binary, octal });
 
-  // Array
+  //! Array
   const hobbies: string[] = ['Reading', 'Traveling', 'Coding'];
 
   console.log('Array', { hobbies });
 
-  // Generic array syntax
+  //! Generic array syntax
   const cities: Array<string> = ['New York', 'Los Angeles', 'Chicago'];
   console.log('Generic array', cities);
 
-  // Mixed array using union type
-  const address: [string, number] = ['123 Main St', 101];
+  //! Mixed array using union type
   const mixedArray: (string | number | boolean)[] = [
     'Hello',
     '42',
@@ -34,13 +33,28 @@ export default function BasicTypes(): void {
     3.14,
     false,
   ];
+  //? Tuple
+  const address: [string, number] = ['123 Main St', 101];
   console.log('Mixed Array', { address, mixedArray });
-  // enums
-  // enum Role {
-  //   Admin,
-  //   User,
-  //   Guest,
-  // }
+
+  //! enums
+  enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+  }
+
+  console.log('enums number', Direction.Up);
+  enum Status {
+    Pending = 'PENDING',
+    InProgress = 'IN_PROGRESS',
+    Completed = 'COMPLETED',
+  }
+  console.log('enums string', Status.Pending);
+
+  const currentStatus: Status = Status.InProgress;
+  console.log('Current status:', currentStatus);
   // Object
   // const userRole: Role = Role.User;
 }
